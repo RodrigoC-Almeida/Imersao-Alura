c3a6906cd0ea63859233d5b278553541ecaff43d
RodrigoC-Almeida <rodrigal80@gmail.com>
2024-09-06 18:53:35 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+function buscar() {
+    // Obtém a seção HTML onde os resultados serão exibidos
+    let section = document.getElementById("resultados-pesquisa");
+
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
+    //console.log(campoPesquisa);
+    
+    if(!campoPesquisa){
+        section.innerHTML="<p>Pesquisa sem dados</P>"
+        return
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase()
+
+    // Inicializa uma string vazia para armazenar os resultados da pesquisa
+    let resultados = "";
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
+
+    // Itera sobre cada elemento (dado) dentro do array 'especies'
+    for (let dado of especies) {
+        titulo = dado.titulo.toLowerCase()
+        descricao = dado.descricao.toLowerCase()
+        tags = dado.tags.toLowerCase()
+        if(titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa) ){
+            resultados += `
+            <div class="item-resultado">
+            <h2>
+                <a href="#" target="_blank">${dado.titulo}</a>
+            </h2>
+            <p class="descricao-meta">${dado.descricao}
+                <a href=${dado.link}
+                   target="_blank">saiba mais</a>
+            </p>
+        </div>
+    `;
+        }
+        if (!resultados) {
+            resultados="<p>Nenhum resultado encontrado</p>"    
+        }
+        
+    }
+
+    // Atribui a string com os resultados ao conteúdo HTML da seção
+    section.innerHTML = resultados;
+}
---
+++ dados.js
@@ -0,0 +1,50 @@
+let especies = [
+    {
+        titulo: "Jataí",
+        descricao: "A Abelha Jataí: Um Tesouro da Natureza. A abelha jataí (Tetragonisca angustula) é uma espécie nativa do Brasil, conhecida por sua docilidade e importância ecológica. Sem ferrão, ela produz um mel de alta qualidade, rico em nutrientes e com propriedades medicinais. A jataí é uma polinizadora eficiente, contribuindo para a reprodução de diversas plantas nativas e cultivadas.",
+        link: "https://pt.wikipedia.org/wiki/Tetragonisca_angustula",
+        tags: "abelha jataí Tetragonisca angustula mel polinizadora sem ferrão ecologia tenho"
+    },
+    {
+        titulo: "Mandaçaia",
+        descricao: "A Abelha Mandaçaia: Um Vigia Bonito. A mandaçaia (Melipona quadrifasciata) é uma abelha social brasileira, conhecida por sua beleza e importância ecológica. Seu nome, de origem indígena, significa vigia bonito, uma referência à abelha que guarda a entrada da colmeia. Com corpo robusto, cabeça e tórax pretos, abdome com faixas amarelas e asas ferrugíneas, a mandaçaia é uma polinizadora eficiente, contribuindo para a reprodução de diversas plantas nativas.",
+        link: "https://pt.wikipedia.org/wiki/Melipona_quadrifasciata",
+        tags: "abelha mandaçaia Melipona quadrifasciata polinizadora sem ferrão ecologia"
+    },
+    {
+        titulo: "Mandaguari",
+        descricao: "A Abelha Mandaguari: Uma Guerreira Alada. A mandaguari (Scaptotrigona postica) é uma abelha sem ferrão nativa do Brasil, conhecida por sua natureza protetora e mel saboroso. Com corpo escuro e listras amarelas no abdômen, a mandaguari é uma polinizadora incansável, desempenhando um papel crucial na reprodução de diversas plantas. Apesar de sua aparência delicada, essa abelha é bastante defensiva de sua colmeia, utilizando suas mandíbulas fortes para morder invasores.",
+        link: "https://pt.wikipedia.org/wiki/Scaptotrigona_postica",
+        tags: "abelha mandaguari Scaptotrigona postica polinizadora sem ferrão ecologia tenho"
+    },
+    {
+        titulo: "Uruçu",
+        descricao: "A Abelha Uruçu: Uma Gigante da Polinização. A uruçu (Melipona scutellaris) é uma das maiores abelhas sem ferrão do Brasil, conhecida por sua produção de mel de alta qualidade e sabor marcante. É uma polinizadora essencial para diversas culturas, como o cacau.",
+        link: "https://pt.wikipedia.org/wiki/Melipona_scutellaris",
+        tags: "abelha uruçu Melipona scutellaris polinizadora sem ferrão mel cacau"
+    },
+    {
+        titulo: "Jandaíra",
+        descricao: "A Abelha Jandaíra: Um Tesouro do Nordeste. A jandaíra (Melipona subnitida) é uma abelha nativa do Nordeste brasileiro, famosa por seu mel de sabor único e alto valor comercial. É uma espécie de grande importância para a biodiversidade local.",
+        link: "https://pt.wikipedia.org/wiki/Melipona_subnitida",
+        tags: "abelha jandaíra Melipona subnitida polinizadora sem ferrão mel Nordeste"
+    },
+    {
+        titulo: "Tiúba",
+        descricao: "A Abelha Tiúba: Uma Construção Perfeita. A tiúba (Melipona fasciculata) é conhecida por construir ninhos com potes de cera perfeitos, utilizados para armazenar mel e pólen. É uma espécie bastante rústica e adaptada a diferentes ambientes.",
+        link: "https://pt.wikipedia.org/wiki/Melipona_fasciculata",
+        tags: "abelha tiúba Melipona fasciculata polinizadora sem ferrão mel cera"
+    },
+    {
+        titulo: "Mirim",
+        descricao: "A Abelha Mirim: Pequena, Mas Valiosa. As abelhas mirins são um grupo diversificado de abelhas sem ferrão, conhecidas por seu pequeno porte e ninhos geralmente construídos em cavidades de árvores. Desempenham um papel fundamental na polinização de diversas plantas.",
+        link: "https://www.epagri.sc.gov.br/index.php/2020/09/22/abelhas-em-ferrao-um-mundo-fascinante-dentro-de-pequenas-caixas/",
+        tags: "abelha mirim polinizadora sem ferrão cavidades de árvores pequeno porte"
+    },
+    {
+        titulo: "Iraí",
+        descricao: "A Abelha Iraí: Uma Construtora Incansável. A iraí (Trigona spinipes) é conhecida por construir ninhos complexos, com diversas camadas e compartimentos. Seu mel possui sabor característico e é bastante apreciado.",
+        link: "https://www.epagri.sc.gov.br/index.php/2020/09/22/abelhas-em-ferrao",
+        tags: "abelha iraí Trigona spinipes polinizadora sem ferrão mel ninhos complexos"
+    }
+];
